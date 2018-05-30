6d4907a30c6748700dd8b2a8d04cd3b5cec3c0d7
victoria-huang <vctrah@gmail.com>
2018-05-30 14:18:44 -0400

    add new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,14 @@
 tops = %w{http://www.elcomalitoamherst.com/image/cache/data/category_23/zaful-women-s-shirts-tank-tops-ribbed-surplice-cropped-cami-tank-top-black-y8u7p4fg-2893-200x200_0.png
 https://images.eastbay.com/pi/2VGL-7D0/large/the-north-face-timber-full-zip-jacket-womens https://imilyclothing.com/wp-content/uploads/2017/12/White-formal-shirt-200x200.png
 http://www.elcomalitoamherst.com/image/cache/data/category_31/zaful-women-s-sweaters-vest-sleeveless-pullover-v-neck-high-low-vest-sweater-gray-k-2829-200x200_0.png
-https://cdn.shopify.com/s/files/1/0005/7137/5663/products/mockup-49102bd2_200x200.png?v=1521386480}
+https://cdn.shopify.com/s/files/1/0005/7137/5663/products/mockup-49102bd2_200x200.png?v=1521386480
+http://www.leocoats.com/image/cache/data/category_9/women-s-tops-and-shirts-silk-long-shirt-by-valentino-shirts-tksphcw-nb3aba8w21b-eq7-1830-200x200_0.jpg
+http://www.ahmetturkmenoglu.com/image/cache/data/category_19/marc-o-polo-short-sleeved-t-shirt-womens-tops-t-shirts-nopedoz-3227-200x200.jpg
+http://www.thisoldhouseproducts.com/image/cache/data/category_8/women-s-tops-replay-silk-top-usugqlg-37992669sg-2145-200x200_0.jpg
+http://www.ahmetturkmenoglu.com/image/cache/data/category_19/marc-o-polo-long-sleeved-t-shirt-womens-tops-t-shirts-kmeihil-3225-200x200.jpg
+http://www.housecleaningservicesseattle.com/image/cache/data/category_11/women-s-tops-calvin-klein-tyler-2-true-icon-w-tank-top-light-grey-heather-nggefkk-1735-200x200_0.jpg
+http://www.housecleaningservicesseattle.com/image/cache/data/category_11/women-s-tops-adidas-oe-wash-w-tank-top-bliss-coral-slamaen-1751-200x200_0.jpg
+http://www.ahmetturkmenoglu.com/image/cache/data/category_17/boss-orange-boss-fine-gauge-knit-jumper-womens-tops-knitwear-qniydul-1872-200x200.jpg}
 
 bottoms = %w{https://www.buckmans.com/files/store/items/md/marmot_pillar_pant_womens_black.jpg
 http://www.paheatingandac.com/image/cache/data/category_13/jacquemus-stretch-twill-straight-leg-pants-women-s-nznnpqqg07ooeu-3398-200x200_0.jpg
@@ -19,29 +26,64 @@ http://www.scottssimpson.com/image/cache/data/category_1/women-zoom-pants-with-c
 https://ae01.alicdn.com/kf/HTB1yDbWLXXXXXXbXpXXq6xXFXXXE/High-Quality-Red-Satin-Skirt-Zipper-Waistline-A-Line-Knee-Length-Pleated-Skirt-Customized-Women-Skirts.jpg_200x200.jpg
 http://www.novalingoes.com/image/cache/data/category_13/prada-printed-pencil-skirt-style-geometric-rise-hue-mid-quirkiness-wear-signature-pa-2457-200x200.jpg
 http://www.drshonapalmer.com/image/cache/data/category_13/20170821100820123762126-200x200_0.jpg
-http://www.qualitycontractordepot.com/image/cache/data/category_15/143fashion-ladies-fashion-harem-style-long-pants-small-royal-blue-women-s-clothin-3991-200x200_0.jpg}
+http://www.qualitycontractordepot.com/image/cache/data/category_15/143fashion-ladies-fashion-harem-style-long-pants-small-royal-blue-women-s-clothin-3991-200x200_0.jpg
+https://www.dhresource.com/200x200s/f2-albu-g6-M00-27-F1-rBVaSFqztB2AT54_AAFO0ALD1jA943.jpg/plus-size-side-flower-embroidered-flare-jeans.jpg
+https://cs1.0ps.us/200-200-ffffff/opplanet-mammut-zephira-pant-women-s-blue-denim-regular-inseam-4.jpg
+http://www.royal-6657.com/image/cache/data/category_5/mansur-gavriel-women-silk-shantung-straight-leg-pants-1041348-wzuhiej-1177-200x200.jpg
+https://ak1.ostkcdn.com/images/products/is/images/direct/5623dddf16cffe82b4a02f73ae6d3e1ac9fa37d9/Alfani-Women%27s-Tribal-Ikat-Print-Jersey-Jogger-Pants.jpg?impolicy=medium&imwidth=200
+http://www.royal-6657.com/image/cache/data/category_5/vivienne-westwood-women-tartan-cotton-pants-1022516-gfxtxki-975-200x200.jpg
+https://cs1.0ps.us/200-200-ffffff/opplanet-marmot-scree-pants-women-s-beetle-green-regular-inseam-4.jpg
+http://www.iconographyby.com/image/cache/data/category_5/hilfiger-collection-workwear-shorts-extfqlj-women-s-shorts-id-rw0rw00554-841-200x200_0.jpg
+http://www.assetservicecenter.com/image/cache/data/category_23/red-valentino-printed-shorts-pr0rf1353ml-ak8-cobalto-women-s-shorts-cupyhnd-3410-200x200_0.jpg
+http://www.joesaver.com/image/cache/data/category_4/paul-and-joe-wrap-effect-cotton-shorts-womens-shorts-erohgsf-no-1011843-346-200x200_0.jpg
+http://www.ryannallen.com/image/cache/data/category_9/frayed-denim-shorts-4772211930092992-antique-rose-women-s-shorts-867-200x200_0.jpg}
 
-dresses = %w{http://www.edp-publishing.co.uk/image/cache/data/category_6/sleeveless-drawstring-waist-sunflower-dress-floral-m-women-s-dresses-8k0wnjxar8qv-1395-200x200_0.png
+dresses = %w{https://gloimg.chinabrands.com/cb/pdm-product-pic/Clothing/2017/04/21/grid-img/1493056922316932912.png
+http://www.edp-publishing.co.uk/image/cache/data/category_6/sleeveless-drawstring-waist-sunflower-dress-floral-m-women-s-dresses-8k0wnjxar8qv-1395-200x200_0.png
 https://d3o2e4jr3mxnm3.cloudfront.net/Womens-Stiped-T-Shirt-Dress_41402_1_lg.png
 https://elanstreet.com/media/catalog/product/cache/1/small_image/400x/9df78eab33525d08d6e5fb8d27136e95/o/p/xop1_6.png.pagespeed.ic.nSR4lzEzyl.png
 http://www.freshgarbage.co.uk/wp-content/uploads/2017/01/dress1.png
-http://www.thebaringpub.co.uk/images/sor2245/New%20V%20Neck%20Sleeveless%20Studded%20Romper%20For%20Cheap%20Outlet%20237.png}
+https://elanstreet.com/media/catalog/product/cache/1/small_image/400x/9df78eab33525d08d6e5fb8d27136e95/o/p/xop1_1.png.pagespeed.ic.MLCDBXSJoR.png
+http://www.stickpng.com/assets/thumbs/580b57fbd9996e24bc43bef7.png
+https://gloimg.yoshop.com/yoshop/pdm-product-pic/Clothing/2017/03/28/goods-img/1527555988387922136.png
+http://www.thebaringpub.co.uk/images/sor2245/New%20V%20Neck%20Sleeveless%20Studded%20Romper%20For%20Cheap%20Outlet%20237.png
+https://elanstreet.com/media/catalog/product/cache/1/small_image/400x/9df78eab33525d08d6e5fb8d27136e95/o/p/xop1_2.png.pagespeed.ic.fRw9IE7-i9.png
+https://i.pinimg.com/originals/b1/bf/a5/b1bfa5fc75fe0a0774c4bcc716f8fed1.png
+https://s-media-cache-ak0.pinimg.com/originals/99/ee/b8/99eeb81b5e57878ce4c4957a011a36af.png
+https://i.pinimg.com/originals/bc/af/15/bcaf15fa596863e57cdab62414d31535.png
+http://cdn3.volusion.com/dcbga.erzet/v/vspfiles/photos/GGT-0875-5T.png?1518788288
+http://theyouwho.blob.core.windows.net/theyouwho/lcx_b_20131127064047.png}
 
-shoes = %w{https://media.gucci.com/style/DarkGray_Center_0_0_800x800/1473872416/456202_K5Z30_8979_003_100_0000_Light-GG-Supreme-tiger-slip-on-sneaker.jpg
-https://i.ebayimg.com/thumbs/images/g/aoIAAOSwKX9as3zK/s-l200.jpg
+shoes = %w{https://i.ebayimg.com/thumbs/images/g/aoIAAOSwKX9as3zK/s-l200.jpg
 http://what-is-fashion.com/6266-48112-home/women-s-glitter-silver-height-increasing-hidden-wedge-insole-high-top-sneakers.jpg
 https://www.dhresource.com/200x200/f2/albu/g5/M00/AF/81/rBVaI1lmSXqADPlWAAD0Q2dT8z8008.jpg
-http://www.ashburyskies.com/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/j/e/jeffrey_campbell-charlin-mustard-top_view-thumb-1_9.jpg}
+https://media.gucci.com/style/DarkGray_Center_0_0_800x800/1473872416/456202_K5Z30_8979_003_100_0000_Light-GG-Supreme-tiger-slip-on-sneaker.jpg
+http://www.ashburyskies.com/media/catalog/product/cache/1/small_image/200x/9df78eab33525d08d6e5fb8d27136e95/j/e/jeffrey_campbell-charlin-mustard-top_view-thumb-1_9.jpg
+http://www.apaacadiana.org/image/cache/data/category_19/no-21-shoes-for-women-designer-sandals-2018-new-products-458082-3kjkltbx-2298-200x200_0.jpg
+https://i.pinimg.com/736x/45/50/90/455090e227ce931249ecc3db465ba5dc--pink-boots-heeled-boots.jpg
+https://s7d5.scene7.com/is/image/TheBradfordExchangeOnline/replatformOverlays?layer=comp&wid=200&hei=200&fmt=jpeg,rgb&qlt=90,1&op_sharpen=1&resMode=bicub&op_usm=0.5,2.0,0,0&iccEmbed=0&$product_id=0121914001
+http://images-nitrosell-com.akamaized.net/product_images/18/4475/thumb-krk_velma_blk_b.jpg
+http://www.thetinnitusfoundation.org/image/cache/data/category_10/isabel-marant-women-dyna-suede-ankle-boots-terracotta-half-sizes-please-take-the-nex-2843-200x200_0.jpg
+http://www.cjredmond.co.uk/image/cache/data/category_23/bohemian-wind-wedge-sandals-thick-soles-fish-mouth-high-heeled-coolslippers-waterpro-2872-200x200_0.jpg
+http://www.mariage-montreal.com/images/GZ/Wedges%20259Giuseppe%20Zanotti%20Jennifer%20Women%20Shoes%20Pink%20Wedges%20Sneakers%20Y68m_1.jpg
+http://www.mariage-montreal.com/images/GZ/Wedges%20262Giuseppe%20Zanotti%20Cruel%20Women%20Shoes%20Gold%20Wedges%20Sneakers%20J96p_1.jpg}
 
-accessories = %w{http://www.freepngimg.com/thumb/bag/33959-6-women-bag-transparent-image-thumb.png
+accessories = %w{https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDWsVIR20oMXYk_3ZVzGsYvys3llPnIo74qzZqajxlA4S0ZBQ5AQ
+https://gloimg.chinabrands.com/cb/pdm-product-pic/Clothing/2017/02/11/grid-img/1515110120996305164.png
+http://www.pngmart.com/files/5/Diamond-Necklace-PNG-Free-Download.png
+http://www.freepngimg.com/thumb/bag/33959-6-women-bag-transparent-image-thumb.png
 http://abbaskets.com/wp-content/uploads/2016/09/Genuine-Leather-Handbag-200x200.png
+http://www.gatorshop.com/wp-content/uploads/2017/06/Florida-Gators-Brady-Bracelet.png
+http://www.chintamanis.in/image/cache/data/product/gold/necklace/mg-5613-400x400.png
+https://www.simongjewelry.com/wp-content/uploads/2016/05/LB2064-Simon-G.-white-and-rose-gold-bangle-bracelet-400x400.png
 https://images.eastbay.com/pi/BH7139/large/adidas-originals-relaxed-strapback-hat-womens
+http://s3.amazonaws.com/matchbookmag-pro/products/medium/1002/pearl-flower-earrings.png?1364816115
 https://i.pinimg.com/originals/f3/bc/56/f3bc560ebecc99443e6b53176a9240bd.png
 http://www.freepngimg.com/thumb/women_bag/12-2-women-bag-png-clipart-thumb.png}
 
 User.create(
-  name: "Cloud",
-  email: "CC@CC.com",
+  name: "Ruby",
+  email: "ruby@cc.com",
   id: 27
 )
 
